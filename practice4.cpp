9ea3dd2b2900cf664e8b671e6aa24ba458c595e3
Kshitiz Saini <kshitizsaini.rtk@gmail.com>
2019-01-20 11:19:50 +0530

    Array & constant

--- practice4.cpp
+++ practice4.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 
+#define MAX 100
+
 using namespace std;
 
-class Name:
+int main()
 {
+  int arr[MAX], sum=0,i;
 
-};
+  cout<<"Enter no. of elements : ";
+  cin>>int count;
+  for(i=0;i<count;i++)
+  {
+    arr[i]=i;
+    sum+=arr[i];
+  }
+  cout<<"Array Sum : "<<sum;
+  return 0;
+}
