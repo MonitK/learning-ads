e1a34ec5f16d142c75c9d98ae9be727cb223aa79
Kshitiz Saini <kshitizsaini.rtk@gmail.com>
2019-01-21 00:11:37 +0530

    Using #define

---
+++ Practice/p1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+#define TWO 2
+#define PI 3.14
+
+int main()
+{
+  int r=10;
+  double peri = TWO * PI *r;
+
+  cout<<"Perimeter : "<<peri<<endl;
+
+  return 0;
+}
