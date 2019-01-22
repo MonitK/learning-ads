9a99b6205e8c77f535c6a75d5b988419f40f6f4f
Kshitiz Saini <kshitizsaini.rtk@gmail.com>
2019-01-22 11:56:46 +0530

    Fixed errors and printed menu

--- lab2b.cpp
+++ lab2b.cpp
@@ -1,11 +1,18 @@
-#inlcude <iostream>
+#include <iostream>
 
 using namespace std;
 
-class Queue;
+class Queue
 {};
 
 int main()
 {
+  cout<<"--------------------------------QUEUE USING ARRAY-------------------------------";
+
+  cout<<endl<<endl<<endl<<endl;
+  cout<<endl<<"1.\tPush elemnt in Stack";
+  cout<<endl<<"2.\tPop elemnt from Stack";
+  cout<<endl<<"3.\tDisplay elemnts in Stack";
+  cout<<endl<<endl<<"0.\tExit";
   return 0;
 }
