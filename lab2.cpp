8cd202fa8ad925aa4f03b2eea6f5b1c0638d9d7e
Kshitiz Saini <kshitizsaini.rtk@gmail.com>
2019-01-22 09:50:06 +0530

    tested parametrised const.

--- lab2.cpp
+++ lab2.cpp
@@ -7,11 +7,12 @@ class Factorial
   public:
     Factorial(int i)
     {
-
-    };
+      cout<<"Parametrised Constructor calling"<<endl;
+    }
 };
 
 int main()
 {
+  Factorial fact(1);
   return 0;
 }
