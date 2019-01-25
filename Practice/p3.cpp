65a0cf3637da3980bbbe7a548ed2dcf9409bf799
Kshitiz Saini <kshitizsaini.rtk@gmail.com>
2019-01-25 23:07:01 +0530

    String functionality added

--- Practice/p3.cpp
+++ Practice/p3.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 int main()
 {
+  char * str = strdup("IIT,Kharagpur");
+  str[0]='N';       //Edit the name
+  cout<<str<<endl;
   return 0;
 }
