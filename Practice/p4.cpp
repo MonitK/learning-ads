4c7e23a9aa67f1a35a053f08119a8f9374674fc1
Kshitiz Saini <kshitizsaini.rtk@gmail.com>
2019-01-25 23:25:53 +0530

    Extention to p3.cpp

--- Practice/p4.cpp
+++ Practice/p4.cpp
@@ -7,6 +7,10 @@ int main()
 {
   const char * str = strdup("IIT,Kharagpur");
   cout<<str<<endl;
-  str[0]='N';
+    //str[0]='N';
+    //Error : Cannot edit the name
+  str = strdup("JIT,Kharagpur");
+    //Change the name
+  cout<<str<<endl;
   return 0;
 }
