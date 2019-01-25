78088966607ada72c59f76ba8e45267d105cc178
Kshitiz Saini <kshitizsaini.rtk@gmail.com>
2019-01-25 23:02:25 +0530

    Added a comment

--- Practice/p2.cpp
+++ Practice/p2.cpp
@@ -8,6 +8,7 @@ int main()
   int m=4;
   const int n=5;
   const int *p=&n;
+    //if *p is not constant then it is wrong because a variable cant point to a constant
   //n=6;
     // n is a constant so cant be changed
   //*p=7;
