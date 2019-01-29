10a2a5e18d1210b906941747777cc0c1e1b9f4af
Kshitiz Saini <kshitizsaini.rtk@gmail.com>
2019-01-29 10:14:01 +0530

    Studies different notations of pointer

--- Practice/class1.cpp
+++ Practice/class1.cpp
@@ -14,10 +14,26 @@ const int SIZE=10;
 int main()
 {
   int a[SIZE];
+  int *ptr=a;
   for (int i=0;i<SIZE;i++)
   {
     a[i]=i;
-    cout<<a[i]<<endl;
   }
+  cout<<*(ptr++);
+  cout<<endl;
+  cout<<*ptr;
+  cout<<endl;
+  cout<<*(++ptr);
+  cout<<endl;
+  cout<<*ptr;
+  cout<<endl;
+  cout<<++(*ptr);
+  cout<<endl;
+  cout<<*ptr;
+  cout<<endl;
+  cout<<(*ptr)++;
+  cout<<endl;
+  cout<<*ptr;
+  cout<<endl;
   return 0;
 }
