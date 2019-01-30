3d76a2719c7d7b30234153e2e40396278b7fd9aa
Kshitiz Saini <kshitizsaini.rtk@gmail.com>
2019-01-30 21:39:36 +0530

    Compilation Error...!

--- Practice/p11.cpp
+++ Practice/p11.cpp
@@ -5,11 +5,16 @@ using namespace std;
 //Refrence parameter as a constant
 
 int Ref (const int &x)
-{}
+{
+  ++x;
+  return (x);
+}
 
 int main()
 {
   int a = 10;
   int b;
+  b = Ref(a);
+  cout<<"a : "<<a<<", b: "<<b<<endl;
   return 0;
 }
