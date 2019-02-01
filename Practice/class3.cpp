2820647e7ae00037f4df1111da066af22776a755
Kshitiz Saini <kshitizsaini.rtk@gmail.com>
2019-02-01 06:39:20 +0530

    Variables declared

--- Practice/class3.cpp
+++ Practice/class3.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main()
 {
+  int choice;
+  int num1,num2;
   cout<<endl<<endl<<"1.Addition"<<endl<<"2. Subtraction"<<endl<<"3. Multiplication"<<endl<<"4. Division"<<endl<<"5. Power"<<endl<<endl<<"0.Exit"<<endl;
 	cout<<endl<<endl;
 	cout<<"Enter your choice : ";
