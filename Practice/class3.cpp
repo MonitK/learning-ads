8454579f9452b6c03eb9c16e01fb20f72f18427c
Kshitiz Saini <kshitizsaini.rtk@gmail.com>
2019-02-01 06:38:27 +0530

    Menu displayed

--- Practice/class3.cpp
+++ Practice/class3.cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 int main()
 {
+  cout<<endl<<endl<<"1.Addition"<<endl<<"2. Subtraction"<<endl<<"3. Multiplication"<<endl<<"4. Division"<<endl<<"5. Power"<<endl<<endl<<"0.Exit"<<endl;
+	cout<<endl<<endl;
+	cout<<"Enter your choice : ";
+	cin>>choice;
+	cout<<endl<<endl;
   return 0;
 }
