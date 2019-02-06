424e16893589068b815077936b44ae5ba80681d9
Kshitiz Saini <kshitizsaini.rtk@gmail.com>
2019-02-06 05:43:53 +0530

    Default parameters used

--- Practice/p12.cpp
+++ Practice/p12.cpp
@@ -2,9 +2,20 @@
 
 using namespace std;
 
+int Identify(int a = 5)//Default value
+{
+  return (a);
+}
+
 int main()
 {
   int a = 10,b;
+  b=Identify(a);
+    //Normal function call
+  cout<<"b : "<<b<<endl<<endl;
+
+  b=Identify();
+    //Default parameters used
   cout<<"b : "<<b<<endl<<endl;
   return 0;
 }
