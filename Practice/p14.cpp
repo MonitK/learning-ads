59c53ab62ceef3ccf2a26dcdb2cb86de37442f5c
Kshitiz Saini <kshitizsaini.rtk@gmail.com>
2019-02-07 18:59:04 +0530

    Initialised variables

--- Practice/p14.cpp
+++ Practice/p14.cpp
@@ -7,11 +7,15 @@ using namespace std;
 
 void g (int, double=0, char * =NULL);
   //OK
+    //All parameters to the right of default parameter must be initialised.
 //void g (int, double=1, char * =NULL);
   //Cant initialise again
 
 int main()
 {
+  int i=5;
+  double d=2.3;
+  char c = 'b';
   cout<<"Default values";
   return 0;
 }
