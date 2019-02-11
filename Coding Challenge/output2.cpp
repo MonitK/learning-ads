8fee32b1d5e5c295a4215175b11a0ebf1c0cc91c
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-02-11 07:57:26 +0530

    Guess the output

--- Coding Challenge/output2.cpp
+++ Coding Challenge/output2.cpp
@@ -7,5 +7,15 @@ int main()
   bool a = true;
   bool b = false;
   bool c = false;
+
+  if(a || b && c)
+  {
+    cout<<"True Block";
+  }
+  else
+  {
+    cout<<"False Block";
+  }
+  
   return 0;
 }
