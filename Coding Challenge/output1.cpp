e8a1cab8139da22a92e7582b7aaeb3e7bb0baa8e
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-02-11 07:47:03 +0530

    Guess the output

--- Coding Challenge/output1.cpp
+++ Coding Challenge/output1.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 int main()
 {
+  String stringone = new String("nextGenCoder");
+  String stringtwo = new String("nextGenCoder");
+
+  if (stringone == stringtwo)
+  {
+    cout<<"Equal";
+  }
+  else
+  {
+    cout<<"Not Equal";
+  }
+
   return 0;
 }
