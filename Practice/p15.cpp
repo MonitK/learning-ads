ff620b2558202bb299febadd654da097138b6f3e
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-02-18 21:37:51 +0530

    Basic c++ programs

--- Practice/p15.cpp
+++ Practice/p15.cpp
@@ -4,10 +4,19 @@ using namespace std;
 
 class Student
 {
-
+private:
+  int n;
+public:
+  void fun()
+  {
+    cout<<"Enter a value1 : ";
+    cin>>n;
+  }
 };
 
 int main()
 {
+  Student obj1;
+  obj1.fun();
   return 0;
 }
