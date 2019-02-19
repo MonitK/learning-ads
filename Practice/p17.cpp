8fd24c285734a6f9662875bb3fed2f404df873c7
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-02-19 14:08:21 +0530

    test case

---
+++ Practice/p17.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class himani
+{
+public:
+  himani ()
+  {
+    cout<<"hello";
+  }
+};
+
+int main()
+{
+  cout<<"hi    "<<endl;
+  himani bansal1;
+  himani bansal2;
+  himani bansal3;
+  return 0;
+}
