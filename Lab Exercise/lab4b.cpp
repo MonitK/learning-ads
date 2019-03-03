64e5614b5e299ce097843ee03be00cffa7477702
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-03-03 08:48:46 +0530

    Functionality to oxygen class

--- Lab Exercise/lab4b.cpp
+++ Lab Exercise/lab4b.cpp
@@ -6,6 +6,21 @@ class hydrogen;
 class sulphur;
 class oxygen;
 
+class oxygen
+{
+	int o_moles;
+	float o_molecules;
+public:
+	void getO()
+	{
+		cout<<"\nMoles of Oxygen - ";
+		cin>>o_moles;
+		cout<<"Weight of Oxygen - ";
+		cin>>o_molecules;
+	}
+	friend void calc(oxygen,hydrogen,sulphur);
+};
+
 int main()
 {
   return 0;
