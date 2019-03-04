9ae026613b1bf05d9e8259bf48ee4da34794e861
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-03-04 08:56:41 +0530

    internal exam class declared

--- Lab Exercise/lab4.cpp
+++ Lab Exercise/lab4.cpp
@@ -25,6 +25,28 @@ public:
 	}
 };
 
+class Internal_Exam:public student
+{
+protected:
+	float imarks[6];
+public:
+	void get_imarks()
+	{
+		for(int i=0;i<6;i++)
+		{
+			cout<<"\nEnter Marks of subject "<<i+1<<" - ";
+			cin>>imarks[i];
+		}
+	}
+	void display_imarks()
+	{
+		for(int i=0;i<6;i++)
+		{
+			cout<<"\nMarks of subject "<<i+1<<" - "<<imarks[i];
+		}
+	}
+};
+
 int main()
 {
   return 0;
