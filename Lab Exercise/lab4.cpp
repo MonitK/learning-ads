475e9e7124920fa3d98684923cc7bbc670994828
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-03-04 08:55:52 +0530

    student class declared

--- Lab Exercise/lab4.cpp
+++ Lab Exercise/lab4.cpp
@@ -2,6 +2,29 @@
 
 using namespace std;
 
+class student
+{
+	int roll;
+	string name;
+	char gender;
+public:
+	void student_Details()
+	{
+		cout<<"\nEnter Name - ";
+		cin>>name;
+		cout<<"\nEnter Roll - ";
+		cin>>roll;
+		cout<<"\nEnter Gender - ";
+		cin>>gender;
+	}
+	void show_student_Details()
+	{
+		cout<<"\nName - "<<name;
+		cout<<"\nRoll - "<<roll;
+		cout<<"\nGender - "<<gender;
+	}
+};
+
 int main()
 {
   return 0;
