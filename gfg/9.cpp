c7e87cfc8282710dff83b16eb0d77e0ce5a59f69
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-03-11 22:26:06 +0530

    Correcting the code

--- gfg/9.cpp
+++ gfg/9.cpp
@@ -2,17 +2,17 @@
 // Run Time Type Identification
 #include<iostream>
 using namespace std;
-class B { };
-class D: public B {};
+class B { virtual void fun() {} };
+class D: public B { };
 
 int main()
 {
     B *b = new D;
     D *d = dynamic_cast<D*>(b);
     if(d != NULL)
-        cout<<"works";
+        cout << "works";
     else
-        cout<<"cannot cast B* to D*";
+        cout << "cannot cast B* to D*";
     getchar();
     return 0;
 }
